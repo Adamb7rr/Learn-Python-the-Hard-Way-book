7839775f53b1d9d9d190737d359e1fc8cf918e91
Adamb7rr <bahrcr7@gmail.com>
2026-07-01 16:36:13 +0300

    Add initial exercise script with print statements

---
+++ Exercises/ex1.py
@@ -0,0 +1,9 @@
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print("Yay! Printing.")
+print("I'd mush rather you 'not'.")
+print('I "said" do not touch this.')
+print("This is mine.")
+# This is comment line
